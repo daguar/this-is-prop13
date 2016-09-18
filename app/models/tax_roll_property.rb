4af64d46e13151220aa475ce3ce9b93df1287348
Dave Guarino <dave@codeforamerica.org>
2016-09-18 15:31:41 -0700

    First pass of address cleaning

--- app/models/tax_roll_property.rb
+++ app/models/tax_roll_property.rb
@@ -1,2 +1,7 @@
 class TaxRollProperty < ActiveRecord::Base
+  def cleaned_address
+    index_of_first_real_character = location_address.index(/[^0\s]/)
+    index_of_last_real_character = location_address.rindex(/[^0\s]/)
+    location_address[index_of_first_real_character..index_of_last_real_character]
+  end
 end
--- spec/models/tax_roll_property_spec.rb
+++ spec/models/tax_roll_property_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe TaxRollProperty, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#cleaned_address' do
+    let(:property) do
+      TaxRollProperty.new(location_address: raw_address)
+    end
+
+    context 'with an address with extraneous zeroes at the beginning' do
+      let(:raw_address) { "0000 0143 JUDSON AV0000" }
+
+      it 'removes them' do
+        expect(property.cleaned_address).to eq("143 JUDSON AV")
+      end
+    end
+  end
 end
